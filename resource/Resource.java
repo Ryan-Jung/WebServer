f6317b0fd7d278a390fb1e1363cd91d70ffd8837
Ryan <ryanjung93@gmail.com>
2018-02-16 11:19:35 -0800

    Fixed resolving path

--- resource/Resource.java
+++ resource/Resource.java
@@ -1,65 +1,96 @@
 package resource;
 import filereaders.*;
+import java.io.File;
+
 public class Resource {
 
   private final String DIRINDEX = "index.html";
   private String absolutePath;
   private boolean isScript = false;
-  private boolean isProtected = false;
+  private HttpdConfig configFile;
 
 
   public Resource(HttpdConfig configFile, String uri) {
+
+    this.configFile = configFile;
     this.absolutePath  = resolveAbsolutePath(uri,configFile);
   }
 
 
   private boolean isFile(String uri) {
+
     if( uri != null && uri.contains(".") ) {
       return true;
     }
+
     return false;
   }
 
+
   private String resolveAbsolutePath(String uri, HttpdConfig configFile) {
-    String docRoot = configFile.getConfigValue("DocumentRoot");
-    docRoot = docRoot.substring(0,docRoot.length()-1); // Remove slash since uri will have one
+
     String absPath = "";
-    if(configFile.getAliasValue(uri) != null) {
-      absPath = configFile.getAliasValue(uri);
-    } else if( configFile.getScriptValue(uri) != null) {
-      absPath = configFile.getScriptValue(uri);
-      isScript = true;
-    } else {
-      if(uri != null && uri.length() > 1)
-        absPath = docRoot + uri.substring(1);
-    }
+    absPath = modifyUri(uri);
+
     if( isFile(uri) == false ) {
       absPath = absPath + DIRINDEX;
     }
+    System.out.println(absPath);
     return absPath;
   }
 
 
-  public String absolutePath(){
+  public String modifyUri(String uri){
+
+    String docRoot = configFile.getConfigValue("DocumentRoot");
+    docRoot = docRoot.substring(0,docRoot.length()-1); // Remove slash since uri will have one
+    String modifiedUri = "";
+    if(configFile.getAliasNameIfAlias(uri).length() > 0){
+
+      String alias = configFile.getAliasNameIfAlias(uri);
+      String aliasValue = configFile.getAliasValue(alias);
+      modifiedUri = uri.replace(alias, aliasValue);
+
+    } else if(configFile.getScriptNameIfScript(uri).length() > 0 ) {
+
+      String scriptAlias = configFile.getScriptNameIfScript(uri);
+      String scriptValue = configFile.getScriptValue(scriptAlias);
+      modifiedUri = uri.replace(scriptAlias,scriptValue);
+      isScript = true;
+
+    } else {
+        modifiedUri = docRoot + uri;
+    }
+    return modifiedUri;
+  }
+
+
+  public String getAbsolutePath(){
+
     return absolutePath;
   }
 
+
   public boolean isScript(String script) {
+
     return isScript;
   }
 
+
   public boolean isProtected() {
-    return isProtected;
+
+    String accesFileName = ".htaccess";
+
+    if(configFile.getConfigValue("AccessFileName") != null){
+      accesFileName = configFile.getConfigValue("AccessFileName");
+    }
+
+    String directoryToCheck = absolutePath.substring(0,absolutePath.lastIndexOf('/') + 1);
+    System.out.println("Diretory:" + directoryToCheck);
+    File accessFile = new File(directoryToCheck + accesFileName);
+
+    return accessFile.exists();
   }
 
-  public static void main(String[] args) {
-
-    HttpdConfig config = new HttpdConfig("conf/httpd.conf");
-    config.load();
-    Resource test = new Resource(config,"/tommySucksDick1.pdf");
-    System.out.println(test.absolutePath());
-
-
-  }
 
 }
