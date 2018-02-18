b340607429ff92c8f2a7055f2a5efc60d7750e6e
Alvin <alvinrobertlee@gmail.com>
2018-02-17 18:10:03 -0800

    removed authentication functions from

--- Worker.java
+++ Worker.java
@@ -21,33 +21,12 @@ public class Worker{
     }
 
     public void run() throws IOException{
-      ResponseFactory responseFactory = new ResponseFactory();
+      ResponseFactory responseFactory = new ResponseFactory(config);
       Request request = new Request(client.getInputStream());
       //request.test();
       Resource requestResource = new Resource(config, request.getUri());
       Response response = responseFactory.getResponse(request,requestResource);
       response.send(client.getOutputStream());
       client.close();
-    }
-
-
-    private boolean htaccessExists(){
-      File htaccessFile = new File(config.getConfigValue("AccessFileName"));
-      return htaccessFile.exists();
-    }
-
-    private boolean hasAuthorization(){
-      Htaccess htaccess = new Htaccess(config.getConfigValue("AccessFileName"));
-      Htpassword htpaswd = htaccess.getAuthUserFile();
-      String authInfo = config.getConfigValue("Authorization");
-
-      String[] authTypeAndCredentials = authInfo.split("//s");
-      String authType = authTypeAndCredentials[0];
-      String authCredentials = authTypeAndCredentials[1];
-
-      if(authType != htaccess.getAuthType()){
-          return false;
-      }
-      return htpaswd.isAuthorized(authCredentials);
     }
 }
