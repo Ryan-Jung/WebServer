a0acc6e90ca4e280530be5a2bee7e5deecc898b9
Ryan <ryanjung93@gmail.com>
2018-02-19 02:30:46 -0800

    Sends Content-Location header

--- response/Response201.java
+++ response/Response201.java
@@ -1,5 +1,7 @@
 package response;
 import resource.Resource;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Response201 extends Response{
     Response201(Resource resource){
@@ -8,5 +10,11 @@ public class Response201 extends Response{
       this.reasonPhrase = "Created";
     }
 
-    
+    @Override
+    public void send(OutputStream outputStream) throws IOException{
+      super.send(outputStream);
+      String contentLocation = "Content-Location: " + resource.getAbsolutePath();
+      addToHeaders(contentLocation.getBytes());
+      write(additionalHeaders, outputStream);
+    }
 }
